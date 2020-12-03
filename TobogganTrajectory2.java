952ea613b81289a57899a549db768db1a3f6e74b
dotslashtaylor <tayloryang.0721@gmail.com>
2020-12-03 13:41:44 +0000

    edit

---
+++ TobogganTrajectory2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class TobogganTrajectory2 {
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(new File("input3"));
+		int counter = 0;
+		int x = 1;
+		String k = "";	
+
+		while (in.hasNextLine()) {
+			k = in.nextLine();
+			if (x > k.length()) {
+				x = x - k.length();
+			}
+
+			if (k.charAt(x - 1) == '#') {
+				counter++;
+			}
+			x += 3;
+		}
+		System.out.println(k.length());
+		System.out.println(counter);
+	}
+}

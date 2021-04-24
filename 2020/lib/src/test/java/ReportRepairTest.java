5e79a090024418c07afaecf6049aaa6f763349e7
taylor y <64106592+tayleyi@users.noreply.github.com>
2021-04-24 14:02:43 -0400

    [04/24] addition: added day three

--- 2020/lib/src/test/java/ReportRepairTest.java
+++ 2020/lib/src/test/java/ReportRepairTest.java
@@ -4,10 +4,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import adventofcode2020.one.ReportRepair;
 
 class ReportRepairTest {
-	@Test void testMainMethods() {
-
+	@Test void testPartOne() {
 		assertEquals(514579, ReportRepair.partOne(new int[]{1721, 979, 366, 299, 675, 1456}));
+	}
+	@Test void testPartTwo() {
 		assertEquals(241861950, ReportRepair.partTwo(new int[]{1721, 979, 366, 299, 675, 1456}));
-
-    }
+	}
 }

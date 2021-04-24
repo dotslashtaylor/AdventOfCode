1e3e7f692b3a5eacd845d8c3d65fb2150a1a9932
taylor y <64106592+tayleyi@users.noreply.github.com>
2021-04-23 22:51:55 -0400

    [04/23] addition: added data files

--- 2020/lib/src/main/java/ui/GetResult.java
+++ 2020/lib/src/main/java/ui/GetResult.java
@@ -5,7 +5,7 @@ import adventofcode2020.ReportRepair;
 public class GetResult {
 
 	public static void main(String[] args) {
-		int[] dataSet = GetData.returnIntegerArray("../../../resources/test/day1.txt");
+		int[] dataSet = ReportRepair.getData("../../../resources/test/day1.txt");
 			int result1 = ReportRepair.partOne(dataSet);
 			System.out.println(result1);
 			int result2 = ReportRepair.partTwo(dataSet);
